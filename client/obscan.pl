2bdff18b6ac59c5f95b72c97b80ed3319204f8ac
k0u5uk3 <mail.to.k0u5uk3@gmail.com>
2015-06-30 16:31:21 +0900

    delete verbose method

--- client/obscan.pl
+++ client/obscan.pl
@@ -7,48 +7,19 @@ use HTTP::Request::Common;
 use LWP::UserAgent;
 use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
 use File::Spec;
-use Data::Dumper;
 use JSON qw(encode_json decode_json);
 use FindBin qw($Bin);
+use Data::Dumper;
 
-our $VERBOSE=0;
 our $YAML = YAML::LoadFile("$Bin/../settings.yaml");
-
-sub usage{
-   printf("Usage : %s -f filename -m detect-obfuscate|detect-webshell|deobfuscate|tracelog|viewfunc [-v]\n", $0); 
-   exit(0);
-}
-
-my %opts;
-GetOptions(\%opts, qw ( 
-   filename|f=s
-   mode|m=s
-   vervbose|v
-));
-
-if(! exists $opts{filename}){
-   #filenameオプションが渡っていないならusageを表示して終了
-   usage();
-}
-
-if(! exists $opts{mode}){
-   #modeオプションが渡っていないならdetectに設定する
-   $opts{mode} = 'detect-webshell';
-}
-
-if(exists $opts{verbose}){
-   #verboseオプションが渡っているな$VERBOSEを真にする。
-   $VERBOSE=1;
-}
-
-sub verbose($){
-   my $msg = shift;
-   printf("[*] $msg\n") if $VERBOSE;
-}
 
 #------------#
 # SUB ROUTIN #
 #------------#
+sub usage{
+   printf("Usage : %s -f filename -m detect-obfuscate|detect-webshell|deobfuscate|tracelog|viewfunc\n", $0); 
+   exit(0);
+}
 
 sub get_md5($){
    my $filename = shift;
@@ -61,6 +32,23 @@ sub get_md5($){
 #-------------#
 # MAIN ROUTIN #
 #-------------#
+
+# オプション解析
+my %opts;
+GetOptions(\%opts, qw ( 
+   filename|f=s
+   mode|m=s
+));
+
+if(! exists $opts{filename}){
+   #filenameオプションが渡っていないならusageを表示して終了
+   usage();
+}
+
+if(! exists $opts{mode}){
+   #modeオプションが渡っていないならdetectに設定する
+   $opts{mode} = 'detect-webshell';
+}
 
 my $target_file = $opts{filename};
 my $abs_filename = File::Spec->rel2abs("$opts{filename}");
