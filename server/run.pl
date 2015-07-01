3552205e6e20e9e9b100d345c5c6f0e9411c9187
k0u5uk3 <mail.to.k0u5uk3@gmail.com>
2015-07-01 14:32:15 +0900

    modified settings script and template file

--- server/run.pl
+++ server/run.pl
@@ -30,12 +30,18 @@ sub generate_from_template($$){
 	close($fh);
 }
 
+sub msg($){
+   my $msg = shift;
+   print "[*] $msg\n";
+}
+
 #-------------#
 # MAIN ROUTIN #
 #-------------#
 sub main(){
-	# 作業上必要なディレクトリを作成する
-	init_dir($YAML->{WEBROOT});
+    # 解析対象PHPスクリプトを配置するディレクトリを作成する
+	init_dir($YAML->{WEBROOT_DIR});
+    # TRACELOG出力先ディレクトリを作成する
 	init_dir($YAML->{TRACELOG_DIR});
 
 	# Templateからphp実行前処理と実行後処理を作成
@@ -46,30 +52,54 @@ sub main(){
 	generate_from_template("./template/custom-php.ini", {CWD => getcwd()});
 
 	# Templateからiptables.ruleを作成
-	generate_from_template("./template/iptables.rule", { PLACK_SERVER_PORT  => "$YAML->{PLACK_SERVER_PORT}" });
+	generate_from_template("./template/iptables.rule", { SANDBOX_HTTPD_PORT  => "$YAML->{SANDBOX_HTTPD_PORT}" });
 
-	print "以下のコマンドでsshとPLACK SERVERT以外の通信を遮断します。\n";
-	print "sudo iptables-restore ./iptables.rule\n";
-    print "元に戻す時は以下の処理をコマンドを使用してください。\n";
-    print "sudo /sbin/iptables -X\n";
-    print "sudo /sbin/iptables -P INPUT ACCEPT\n";
-    print "sudo /sbin/iptables -P OUTPUT ACCEPT\n";
-    print "sudo /sbin/iptables -P FORWARD ACCEPT\n";
-    print "sudo /sbin/iptables -F\n";
+    # iptables設定の指示
+	msg("以下のコマンドでSANDBOX_HTTPDとSSH以外の通信を遮断します。");
+	msg("sudo iptables-restore ./iptables.rule");
+    msg("iptables設定をデフォルトに戻すには以下のコマンドを入力してください");
+    msg("sudo /sbin/iptables -X");
+    msg("sudo /sbin/iptables -P INPUT ACCEPT");
+    msg("sudo /sbin/iptables -P OUTPUT ACCEPT");
+    msg("sudo /sbin/iptables -P FORWARD ACCEPT");
+    msg("sudo /sbin/iptables -F");
 
-	if($YAML->{USING_HTTPS}){
-   	  # HTTPS対応
-   	  print "HTTPS対応のために秘密鍵、公開鍵、証明書を作成します。\n";
-   	  system("openssl genrsa 2048 > server.key");
+    if($YAML->{USE_SSL}){
+      msg("HTTPSを使用するために秘密鍵、公開鍵、証明書を作成します。");
+ 	  system("openssl genrsa 2048 > server.key");
    	  system("openssl req -new -key server.key -out server.csr -subj '/C=JP/ST=Tokyo/L=Tokyo/O=Example Ltd./OU=Web/CN=example.com'");
    	  system("openssl x509 -in server.csr -days 365 -req -signkey server.key > server.crt");
-   	  system("/usr/bin/plackup -s Starman -a observ.psgi --ssl-key-file server.key --ssl-cert-file server.crt --ssl 1 --host $YAML->{PLACK_SERVER_HOST} --port $YAML->{PLACK_SERVER_PORT} >> $YAML->{PLACK_SERVER_LOG} 2>&1 &");
-	}else{
-	  system("/usr/bin/plackup -s Starman -a observ.psgi --host $YAML->{PLACK_SERVER_HOST} --port $YAML->{PLACK_SERVER_PORT} >> $YAML->{PLACK_SERVER_LOG} 2>&1 &");
-	}
+    } 
+
+    if($YAML->{SANDBOX_HTTPD_ENGINE} eq 'APACHE'){
+
+    }elsif($YAML->{SANDBOX_HTTPD_ENGINE} eq 'PLACK'){
+      # HTTPD_ENGINEにPLACKを使用する
+      if($YAML->{USE_SSL}){
+         # PLACKをHTTPSプロトコルで立ち上げる
+         system("/usr/bin/plackup -s HTTP::Server::PSGI --ssl-key-file server.key " . 
+                "--ssl-cert-file server.crt --ssl 1 observ.psgi " . 
+                "--host $YAML->{SANDBOX_HTTPD_HOST} --port $YAML->{SANDBOX_HTTPD_PORT} >> $YAML->{SANDBOX_HTTPD_LOG} 2>&1 &"); 
+      }else{
+         # PLACKをHTTPプロトコルで立ち上げる
+         system("/usr/bin/plackup observ.psgi --host $YAML->{SANDBOX_HTTPD_HOST} " . 
+                "--port $YAML->{SANDBOX_HTTPD_PORT} >> $YAML->{SANDBOX_HTTPD_LOG} 2>&1 &");
+      }
+    }elsif($YAML->{SANDBOX_HTTPD_ENGINE} eq 'STARMAN'){
+      # HTTPD_ENGINEにSTARMANを使用する
+      if($YAML->{USE_SSL}){
+   	     system("/usr/bin/plackup -s Starman -a observ.psgi --ssl-key-file server.key " . 
+                "--ssl-cert-file server.crt --ssl 1 " . 
+                "--host $YAML->{SANDBOX_HTTPD_HOST} --port $YAML->{SANDBOX_HTTPD_PORT} >> $YAML->{SANDBOX_HTTPD_LOG} 2>&1 &");
+      }else{
+	     system("/usr/bin/plackup -s Starman -a observ.psgi " . 
+                "--host $YAML->{SANDBOX_HTTPD_HOST} --port $YAML->{SANDBOX_HTTPD_PORT} >> $YAML->{SANDBOX_HTTPD_LOG} 2>&1 &");
+      }
+    }
 
 	# plackとphp builid in serverの起動
-	system("/usr/bin/php -t $YAML->{WEBROOT} -S $YAML->{PHP_BUILD_SERVER_HOST}:$YAML->{PHP_BUILD_SERVER_PORT} -c ./custom-php.ini >> $YAML->{PHP_BUILD_SERVER_LOG} 2>&1 &");
+	system("/usr/bin/php -t $YAML->{WEBROOT} -S $YAML->{PHP_BUILTIN_SERVER_HOST}:$YAML->{PHP_BUILTIN_SERVER_PORT} " . 
+           "-c ./custom-php.ini >> $YAML->{PHP_BUILTIN_SERVER_LOG} 2>&1 &");
 }
 
 main ();
